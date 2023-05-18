276fefbfff6ac539a06ec74b19a95bccb730046c
Arild M Johannessen <arildj78@gmail.com>
2023-05-18 19:45:29 +0200

    Improvement in readability of output

--- findUSBdevice.py
+++ findUSBdevice.py
@@ -6,8 +6,8 @@ def listUSBdevices():
     # find USB devices
     for dev in libusb_package.find(find_all=True):
     # loop through devices, printing vendor and product ids in decimal and hex
-        sys.stdout.write('Decimal VendorID=' + str(dev.idVendor) + ' & ProductID=' + str(dev.idProduct) + '\n')
-        sys.stdout.write('Hexadecimal VendorID=' + hex(dev.idVendor) + ' & ProductID=' + hex(dev.idProduct) + '\n\n')
+        #sys.stdout.write('Decimal VendorID=' + str(dev.idVendor) + ' & ProductID=' + str(dev.idProduct) + '\n')
+        sys.stdout.write('VendorID = ' + hex(dev.idVendor) + '\t' + 'ProductID = ' + hex(dev.idProduct) + '\n')
 
     
   
