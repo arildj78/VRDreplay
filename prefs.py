9629a8cc1c20738695b17d5dcc2e0d2206d8c54a
Arild M Johannessen <arildj78@gmail.com>
2024-06-14 22:12:30 +0200

    Updated recording directory

--- prefs.py
+++ prefs.py
@@ -54,8 +54,7 @@ class MarkerColor():
 
 
 RECORDING_DIRECTORIES = [
-    #'F:\\AW101\\Disk1\\vrd_database' ,
-    'F:\\AW101\\tempCopy' ,
+    'D:\\vrd_database' ,
 ]
 # Match filenames of the following format EO_ACT_0000_000.mkv (The $ ensures that the filename ends with .mkv)
 
