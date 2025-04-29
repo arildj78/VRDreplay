747b64fbfffa0e228067785d83aecfb90929060b
Arild M Johannessen <arildj78@gmail.com>
2025-04-29 10:18:19 +0200

    Experimental fix of issue 6 - Import fails when timeline does not return any quad clips when looking to create compound clips.

--- Import_VRD.py
+++ Import_VRD.py
@@ -350,50 +350,56 @@ def createProject(memo=None):
             mcc_clip_info       = {"name" : prefs.MCC_TRACK[1]    + '_' + tl.name}
             eo_opp_clip_info    = {"name" : prefs.EO_OPP_TRACK[1] + '_' + tl.name}
             eo_act_clip_info    = {"name" : prefs.EO_ACT_TRACK[1] + '_' + tl.name}
-            quad_compound = tl.timeline.CreateCompoundClip(quadtrack_items, quadtrack_clip_info)
             tl.timeline.CreateCompoundClip(mcc_items, mcc_clip_info)
             tl.timeline.CreateCompoundClip(eo_opp_items, eo_opp_clip_info)
             tl.timeline.CreateCompoundClip(eo_act_items, eo_act_clip_info)
 
-            #Prepare quad data for duplication into separate tracks
-            quad_start_frame = quad_compound.GetStart()
-            quad_mediapool_item = quad_compound.GetMediaPoolItem()
-            
-            #Add Hoist cam track (duplicate of quad, cropped and scaled)
-            hoist_clip = {
-                    "mediaPoolItem" : quad_mediapool_item,     #The media file to be inserted
-                    "trackIndex" : prefs.HOISTCAM_TRACK[0],    #Track to insert the media in              e.g. 4
-                    "recordFrame" : quad_start_frame           #The timeline location (in frames) to insert the clip at       e.g. 1 239 324 + 6000
-                    }
-            hoist_timelineitem = mediaPool.AppendToTimeline( [hoist_clip] )
-            hoist_timelineitem[0].SetProperty({'Pan': -287.0,         'Tilt': -1079.0,
-                                               'ZoomX': 0.65,        'ZoomY': 0.65,
-                                               'AnchorPointX': -673.0, 'AnchorPointY': 540.0,
-                                               'RotationAngle': 90.0,
-                                               'CropLeft': 256.0,
-                                               'CropRight': 1012.0,
-                                               'CropTop': 0.0,
-                                               'CropBottom': 548.0}
-                                               )
+            if len(quadtrack_items) > 0:
+                try:
+                    quad_compound = tl.timeline.CreateCompoundClip(quadtrack_items, quadtrack_clip_info)
 
-            
-            #Add OPLS track (duplicate of quad, cropped and scaled)
-            opls_clip = {
-                    "mediaPoolItem" : quad_mediapool_item,     #The media file to be inserted
-                    "trackIndex" : prefs.OPLS_TRACK[0],        #Track to insert the media in              e.g. 4
-                    "recordFrame" : quad_start_frame           #The timeline location (in frames) to insert the clip at       e.g. 1 239 324 + 6000
-                    }
-            opls_timelineitem = mediaPool.AppendToTimeline( [opls_clip] )            
-            opls_timelineitem[0].SetProperty({'Pan': -1122.0,           'Tilt': 324.0,
-                                              'ZoomX': 0.690,        'ZoomY': 0.690,
-                                              'AnchorPointX': 334.0, 'AnchorPointY': -258.0,
-                                              'RotationAngle': 0.0,
-                                              'CropLeft': 1083.0,
-                                              'CropRight': 409.0,
-                                              'CropTop': 537.0,
-                                              'CropBottom': 9.0,
-                                              'CompositeMode': resolve.COMPOSITE_SCREEN }
-                                              )
+                    #Prepare quad data for duplication into separate tracks
+                    quad_start_frame = quad_compound.GetStart()
+                    quad_mediapool_item = quad_compound.GetMediaPoolItem()
+                    
+                    #Add Hoist cam track (duplicate of quad, cropped and scaled)
+                    hoist_clip = {
+                            "mediaPoolItem" : quad_mediapool_item,     #The media file to be inserted
+                            "trackIndex" : prefs.HOISTCAM_TRACK[0],    #Track to insert the media in              e.g. 4
+                            "recordFrame" : quad_start_frame           #The timeline location (in frames) to insert the clip at       e.g. 1 239 324 + 6000
+                            }
+                    hoist_timelineitem = mediaPool.AppendToTimeline( [hoist_clip] )
+                    hoist_timelineitem[0].SetProperty({'Pan': -287.0,         'Tilt': -1079.0,
+                                                    'ZoomX': 0.65,        'ZoomY': 0.65,
+                                                    'AnchorPointX': -673.0, 'AnchorPointY': 540.0,
+                                                    'RotationAngle': 90.0,
+                                                    'CropLeft': 256.0,
+                                                    'CropRight': 1012.0,
+                                                    'CropTop': 0.0,
+                                                    'CropBottom': 548.0}
+                                                    )
+
+                    
+                    #Add OPLS track (duplicate of quad, cropped and scaled)
+                    opls_clip = {
+                            "mediaPoolItem" : quad_mediapool_item,     #The media file to be inserted
+                            "trackIndex" : prefs.OPLS_TRACK[0],        #Track to insert the media in              e.g. 4
+                            "recordFrame" : quad_start_frame           #The timeline location (in frames) to insert the clip at       e.g. 1 239 324 + 6000
+                            }
+                    opls_timelineitem = mediaPool.AppendToTimeline( [opls_clip] )            
+                    opls_timelineitem[0].SetProperty({'Pan': -1122.0,           'Tilt': 324.0,
+                                                    'ZoomX': 0.690,        'ZoomY': 0.690,
+                                                    'AnchorPointX': 334.0, 'AnchorPointY': -258.0,
+                                                    'RotationAngle': 0.0,
+                                                    'CropLeft': 1083.0,
+                                                    'CropRight': 409.0,
+                                                    'CropTop': 537.0,
+                                                    'CropBottom': 9.0,
+                                                    'CompositeMode': resolve.COMPOSITE_SCREEN }
+                                                    )
+                except:
+                    pass
+
 
 
             #Add Tail track (duplicate of quad, cropped and scaled)
@@ -412,6 +418,8 @@ def createProject(memo=None):
                                               'CropTop': 537.0,
                                               'CropBottom': 12.0 }
                                               )
+
+
 
             #Add time of day on Time Track
             tod_mediaPool_item = CreateTimeOfDayClip(proj)
